e1f2a88aa5f13d7a3c1ac44889650f57ca3b540e
Sachin Gupta <saching@microsoft.com>
2019-07-17 10:00:13 -0400

    avoid recreating function on each component update

--- master-classes/react-week-performance/avoiding-re-renders/src/App.js
+++ master-classes/react-week-performance/avoiding-re-renders/src/App.js
@@ -16,8 +16,21 @@ class App extends Component {
       dataList: ['one', 'two', 'three', 'four', 'five'],
       toggler: false,
     };
+
+    this.handleTogglerOnClick = this.handleTogglerOnClick.bind(this);
+    /**
+     * Comment out the below method bind and bind it in the render method to
+     * see how that would cause a new function to be made for every render, hence
+     * causing unnecessary re-renders in AddItemForm.jsx when the *new* method is
+     * passed down.
+     */
+    this.handleAddItemSubmit = this.handleAddItemSubmit.bind(this);
   }
 
+  /**
+   * The only purpose for state.toggler and this method is to change some irrelevant state
+   * to demonstrate unnecessary re-renders.
+   */
   handleTogglerOnClick() {
     this.setState(prevState => {
       return {
@@ -30,9 +43,22 @@ class App extends Component {
     const newItem = event.target.newItem.value;
     event.preventDefault();
 
+    /**
+     * Using push here would mutate the dataList array in place. Then, when everything
+     * re-renders, shouldComponentUpdate and PureComponent wouldn't re-render because
+     * the dataList array would be the same, since it's passed by reference and that's
+     * what gets compared. We have to create a *new* array and set our state to that.
+     * This is important. Always think IMMUTABILITY.
+     */
     this.setState(prevState => {
-      const newList = prevState.dataList.concat(newItem);
-      return { dataList: newList };
+      /**
+       *  Try un-commenting the following and commenting out the immutable way to see how
+       * the app breaks. Don't do this.
+       */
+      // prevState.dataList.push(newItem);
+      // return { dataList: prevState.dataList };
+
+      return { dataList: prevState.dataList.concat(newItem) };
     });
 
     event.target.newItem.value = '';
@@ -51,15 +77,15 @@ class App extends Component {
           <p className="toggler">Pointless state: {toggler.toString()}</p>
           <input
             type="button"
-            onClick={this.handleTogglerOnClick.bind(this)}
+            onClick={this.handleTogglerOnClick}
             value="Toggle Pointless State"
           />
-          <AddItemForm handleSubmit={this.handleAddItemSubmit.bind(this)} />
+          <AddItemForm handleSubmit={this.handleAddItemSubmit} />
           <div className="listsWrapper">
             <List items={dataList} />
-            {/* <ListSCU items={dataList} /> */}
-            {<ListPure items={dataList} />}
-            {/* <ListFunctional items={dataList} /> */}
+            <ListSCU items={dataList} />
+            <ListPure items={dataList} />
+            <ListFunctional items={dataList} />
           </div>
         </main>
       </div>
