bcb2f84b3e3260715aafcdbbcb27be1d353149b0
Sachin Gupta <saching@microsoft.com>
2019-07-17 11:35:03 -0400

    img with width and height to avoid recalculate space

--- master-classes/react-week-performance/lazy-image/src/components/LazyImage.jsx
+++ master-classes/react-week-performance/lazy-image/src/components/LazyImage.jsx
@@ -4,7 +4,7 @@ import ViewportObserver from './ViewportObserver';
 const LazyImage = ({ src }) => (
   <ViewportObserver render=
     {(hasBeenInViewport, domRef) => (
-      <img ref={domRef} src={hasBeenInViewport ? src : null/* you can render loading indicator here*/} alt="abstract photograph" />
+      <img ref={domRef} src={hasBeenInViewport ? src : null/* you can render loading indicator here*/} alt="abstract photograph" height="400" />
     )}
   />
 );
