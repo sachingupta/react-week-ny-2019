ffdb644dbe8129eb16f6f1eaefab8590ba539199
Sachin Gupta <saching@microsoft.com>
2019-07-17 15:35:02 -0400

    session_02

---
+++ master-classes/food-state-of-mind-starter/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Router} from '@reach/router';
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import Burgers from "./components/pages/Burgers";
+import Header from "./components/shared/Header";
+import {useAuth} from "./redux/selectors/useAuth";
+
+function App() {
+  const {currentUser} = useAuth();
+
+  return (
+        <>
+        {currentUser && <Header />}
+        <Router>
+          <Login path={'/'}/>
+          <Register path={'/register'}/>
+          <Home path={'/home'}/>
+          <Burgers path={'/burgers'}/>
+        </Router>
+      </>
+  );
+}
+
+export default App;
