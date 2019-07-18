1aa49d20958deabe175119cf4717c06be4ebdcef
Sachin Gupta <saching@microsoft.com>
2019-07-18 14:14:58 -0400

    updating currency app

--- workshop-graphql/apollo-currency-example/src/App.js
+++ workshop-graphql/apollo-currency-example/src/App.js
@@ -2,25 +2,54 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { gql } from "apollo-boost";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, Query  } from "react-apollo";
 
-export default App;
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io"
+});
+
+client
+  .query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
+  const ExchangeRates = () => (
+    <Query
+      query={gql`
+        {
+          rates(currency: "USD") {
+            currency
+            rate
+          }
+        }
+      `}
+    >
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error :(</p>;
+  
+        return data.rates.map(({ currency, rate }) => (
+          <div key={currency}>
+            <p>{currency}: {rate}</p>
+          </div>
+        ));
+      }}
+    </Query>
+  );
+
+  export const ApolloApp = () => (
+    <ApolloProvider client={client}>
+      <div>
+       <ExchangeRates />
+      </div>
+    </ApolloProvider>
+  );
--- workshop-graphql/apollo-currency-example/src/index.js
+++ workshop-graphql/apollo-currency-example/src/index.js
@@ -1,60 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {ApolloApp} from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { gql } from "apollo-boost";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider, Query  } from "react-apollo";
 
-const client = new ApolloClient({
-  uri: "https://48p1r2roz4.sse.codesandbox.io"
-});
-
-client
-  .query({
-    query: gql`
-      {
-        rates(currency: "USD") {
-          currency
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
-
-  const ExchangeRates = () => (
-    <Query
-      query={gql`
-        {
-          rates(currency: "USD") {
-            currency
-            rate
-          }
-        }
-      `}
-    >
-      {({ loading, error, data }) => {
-        if (loading) return <p>Loading...</p>;
-        if (error) return <p>Error :(</p>;
-  
-        return data.rates.map(({ currency, rate }) => (
-          <div key={currency}>
-            <p>{currency}: {rate}</p>
-          </div>
-        ));
-      }}
-    </Query>
-  );
-
-  const ApolloApp = () => (
-    <ApolloProvider client={client}>
-      <div>
-       <ExchangeRates />
-      </div>
-    </ApolloProvider>
-  );
 
 ReactDOM.render(<ApolloApp />, document.getElementById('root'));
 
