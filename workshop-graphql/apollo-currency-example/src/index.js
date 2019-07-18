81cf0d10c1e08d5c6db538f877dfc57314ee1ba3
Sachin Gupta <saching@microsoft.com>
2019-07-18 13:56:28 -0400

    updating lab 1

--- workshop-graphql/apollo-currency-example/src/index.js
+++ workshop-graphql/apollo-currency-example/src/index.js
@@ -4,7 +4,59 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { gql } from "apollo-boost";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, Query  } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io"
+});
+
+client
+  .query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
+  const ExchangeRates = () => (
+    <Query
+      query={gql`
+        {
+          rates(currency: "USD") {
+            currency
+            rate
+          }
+        }
+      `}
+    >
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error :(</p>;
+  
+        return data.rates.map(({ currency, rate }) => (
+          <div key={currency}>
+            <p>{currency}: {rate}</p>
+          </div>
+        ));
+      }}
+    </Query>
+  );
+
+  const ApolloApp = () => (
+    <ApolloProvider client={client}>
+      <div>
+       <ExchangeRates />
+      </div>
+    </ApolloProvider>
+  );
+
+ReactDOM.render(<ApolloApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
